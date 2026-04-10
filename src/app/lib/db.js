b1d8ebde351235864dea77f1dd1b299865b35408
Muhammad Rifky <muh.rifky.harto@gmail.com>
2026-04-10 22:02:48 +0700

    fix db connection

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "76.13.192.195",
-  user: "iotuser",
-  password: "123456",
-  database: "iot_system",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
